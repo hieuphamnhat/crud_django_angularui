26c1eec7d5f81366e56b2863afabc3aa51fa4ae2
hieupn <phamnhathieu_t62@hus.edu.vn>
2021-05-15 21:29:33 +0700

    add company

--- src/app/company/add-edit-comp/add-edit-comp.component.ts
+++ src/app/company/add-edit-comp/add-edit-comp.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {SharedService} from 'src/app/shared.service';
 @Component({
   selector: 'app-add-edit-comp',
   templateUrl: './add-edit-comp.component.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditCompComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+
+  @Input() comp:any;
+  symbol:any;
+  price:any;
+  date:any;
 
   ngOnInit(): void {
+    this.symbol = this.comp.symbol;
+    this.price = this.comp.price;
+    this.date = this.comp.date;
+  }
+  addCompany(){
+    var val = {symbol: this.symbol, 
+              price: this.price,
+              date: this.date}
+    this.service.addComp(val).subscribe(res=>{
+      alert(res.toString());
+    });
   }
 
+  editCompany(){
+    var val = {
+      symbol: this.symbol, 
+      price: this.price,
+      date: this.date}
+    this.service.updateComp(val).subscribe(res=>{
+      alert(res.toString());
+    });
+  }
 }
--- src/app/company/show-comp/show-comp.component.ts
+++ src/app/company/show-comp/show-comp.component.ts
@@ -11,10 +11,33 @@ export class ShowCompComponent implements OnInit {
   constructor(private service:SharedService) { }
 
   CompanyList:any = [];
+  ModalTitle:string="";
+  ActivateAddEditCompany:boolean=false;
+  comp:any= [];
   ngOnInit(): void {
     this.refreshCompList();
   }
+  addClick(){
+    this.comp={
+      symbol:"",
+      price: 0,
+      date: ""
+    }
+    this.ModalTitle = "Add Company";
+    this.ActivateAddEditCompany = true;
+  }
 
+  closeClick(){
+    this.ActivateAddEditCompany = false;
+    this.refreshCompList();  
+  }
+
+  editClick(item:any){
+    this.comp = item;
+    this.ModalTitle = "Edit Company";
+    this.ActivateAddEditCompany=true;
+  }
+  
   refreshCompList(){
     this.service.getCompList().subscribe(data=>{
       this.CompanyList = data;
